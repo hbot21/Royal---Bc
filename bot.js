c8c6bf218eb6c5319b632feac52ed80bc04dafe7
hbot21 <43694980+hbot21@users.noreply.github.com>
2018-10-07 19:52:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on("message", message => {
 
 
 client.on('message', message => {
-    var prefix = ".";
+    var prefix = "!";
     
       if (!message.content.startsWith(prefix)) return;
       var args = message.content.split(' ').slice(1);
